8c1b6b4e4dde1d5a02da48fd0a6dbcaff7a51ed9
unknown <hanane.alaoui.foreach@gmail.com>
2021-12-08 16:08:31 +0100

    yyy

--- formulair.php
+++ formulair.php
@@ -1,30 +1,30 @@
 
 <?php
 session_start();
-require_once "ressources.php";
-require_once "fonctions.php";
+    require_once "ressources.php";
+    require_once "fonctions.php";
                
-    if ($_GET)
-    {
-        $nom = $_GET[ 'Pseudo' ];
-     
+            if ($_GET)
+            {
+                $nom = $_GET[ 'Pseudo' ];
+            
 
-        $mysqli = new mysqli($servername, $username, $password, $database);
-        $query  = "insert into utilisateur_inscrit ( pseudo ) values (' $nom' );";
-        //print( $query );
-        $res = $mysqli->query( $query );
+                $mysqli = new mysqli($servername, $username, $password, $database);
+                $query  = "insert into utilisateur_inscrit ( pseudo ) values (' $nom' );";
+                //print( $query );
+                $res = $mysqli->query( $query );
 
-        if ($res)
-        // {
-        //     if ($res->num_rows == 0)
-         print("<h3>Vous etes bien inscrit pour le tirage au sort</h3>");
-        
-        // }
-        // else
-        //    print("<h3>erreur</h3>");
+                if ($res)
+                // {
+                //     if ($res->num_rows == 0)
+                print("<h3>Vous etes bien inscrit pour le tirage au sort</h3>");
+                
+                // }
+                // else
+                //    print("<h3>erreur</h3>");
 
-        $mysqli->close();
-    }
+                $mysqli->close();
+            }
 ?>
 
 <!DOCTYPE html>
@@ -55,35 +55,32 @@ body
     
 }
 </style>
+<!--javascripte -->
 <script>
-
-document.getElementById( 'butok').disabled= true;
-
-
-function verifie()
-{
-    if ( document.getElementById( 'accept').checked  )
-        document.getElementById( 'butok').disabled= false;
-    else
         document.getElementById( 'butok').disabled= true;
-        
-}
-
+        function verifie()
+        {
+            if ( document.getElementById( 'accept').checked  )
+                document.getElementById( 'butok').disabled= false;
+            else
+                document.getElementById( 'butok').disabled= true;
+                
+        }
 
 </script>
 
 <body background="Noel98.gif">
    
-      <form action="#" method="get">
+        <form action="#" method="get">
 
                 <label for="Pseudo"><b>Pseudo</b></label>
                 <input type="Pseudo" placeholder="Enter Pseudo" name="Pseudo" id="Pseudo" required>
                     <br>
                     <br>
-            <input type="checkbox" name="NomCadeau" id="accept" onclick="verifie()"> 
-            <label for="Validation"><b>Validation</b></label>
-
-                <br>
+                <input type="checkbox" name="NomCadeau" id="accept" onclick="verifie()"> 
+                <label for="Validation"><b>Validation</b></label>
+                    <br>
+                    <br>
                 <button type="submit" class="registerbtn" id="butok" disabled="true">OK</button>
         </form>
 </body>
