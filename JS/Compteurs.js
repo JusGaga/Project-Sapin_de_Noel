74c1e6efb550f8e8735e365aec1238b7c00b2a82
carliolivier <93927234+carliolivier@users.noreply.github.com>
2021-12-08 11:18:07 +0100

    js

--- JS/Compteurs.js
+++ JS/Compteurs.js
@@ -3,30 +3,37 @@ let hoursDiv = document.querySelector("#hours")
 let minutesDiv = document.querySelector("#minutes")
 let secondsDiv = document.querySelector("#seconds")
 
-let date_final = "24 Dec 2021"
+let date_final = document.getElementById("date")
+let valider = document.getElementById("valider")
 
+let date_compteur = ""
 
-function countdown(){
-    let date = new Date(date_final)
+valider.addEventListener("click", e => {
+    e.preventDefault()
+    date_compteur = date_final.value
+})
+
+function countdown() {
+    let date = new Date(date_compteur)
     let current_date = new Date()
 
     // console.log("date",date,"current_date",current_date)
 
-    total_seconds = (date-current_date)/1000
+    total_seconds = (date - current_date) / 1000
 
-    const days = Math.floor(total_seconds/ 3600 / 24)
-    const hours = Math.floor(total_seconds/ 3600) % 24
-    const minutes = Math.floor(total_seconds/ 60) % 60
+    const days = Math.floor(total_seconds / 3600 / 24)
+    const hours = Math.floor(total_seconds / 3600) % 24
+    const minutes = Math.floor(total_seconds / 60) % 60
     const seconds = Math.floor(total_seconds) % 60
 
-    daysDiv.innerHTML = days
-    hoursDiv.innerHTML = formatTime(hours)
-    minutesDiv.innerHTML = formatTime(minutes)
-    secondsDiv.innerHTML = formatTime(seconds)
+    daysDiv.innerText = days
+    hoursDiv.innerText = formatTime(hours)
+    minutesDiv.innerText = formatTime(minutes)
+    secondsDiv.innerText = formatTime(seconds)
 }
 
-function formatTime(time){
-    return time < 10 ? `0${time}` : time; 
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
 }
 
 countdown()
