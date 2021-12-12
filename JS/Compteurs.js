aa6c5f6209028e721dad80467c00b1bf59064a64
Gaetan Jus <gaetan3107@gmail.com>
2021-12-12 17:12:40 +0100

    Revert "Merge branch 'main' of https://github.com/JusGaga/Sapin_de_Noel into FirstThemes"
    This reverts commit 9739857eba4da34b16fc9e926daf7df016a61bdb, reversing
    changes made to 2e955319bcc34c70ec1d314ecbff04e7e4a750f1.

--- JS/Compteurs.js
+++ JS/Compteurs.js
@@ -1,58 +1,34 @@
-let date_final
+let daysDiv = document.querySelector("#days")
+let hoursDiv = document.querySelector("#hours")
+let minutesDiv = document.querySelector("#minutes")
+let secondsDiv = document.querySelector("#seconds")
+
+let date_final = "24 Dec 2021"
 
 
-let date_compteur = ""
-
-
-
-
-function clik() {
-
-    console.log("ok")
-    date_compteur = date_final.value
-    console.log(date_final.value)
-    setInterval(countdown, 1000)
-
-}
-
-
-function init() {
-    console.log("coucou init")
-    // daysDiv = document.getElementById("days")
-    // hoursDiv = document.getElementById("hours")
-    // minutesDiv = document.getElementById("minutes")
-    // secondsDiv = document.getElementById("seconds")
-    console.log(daysDiv)
-
-    date_final = document.getElementById("date")
-    valider = document.getElementById("valider")
-
-    date_compteur = ""
-}
-
-function countdown() {
-    let date = new Date(String(date_compteur))
+function countdown(){
+    let date = new Date(date_final)
     let current_date = new Date()
-    console.log(String(date))
 
     // console.log("date",date,"current_date",current_date)
 
-    total_seconds = (date - current_date) / 1000
-    console.log(total_seconds)
+    total_seconds = (date-current_date)/1000
 
-    const days = Math.floor(total_seconds / 3600 / 24)
-    const hours = Math.floor(total_seconds / 3600) % 24
-    const minutes = Math.floor(total_seconds / 60) % 60
+    const days = Math.floor(total_seconds/ 3600 / 24)
+    const hours = Math.floor(total_seconds/ 3600) % 24
+    const minutes = Math.floor(total_seconds/ 60) % 60
     const seconds = Math.floor(total_seconds) % 60
 
-    console.log(days, hours, minutes, seconds)
-    daysDiv.innerHTML = String(days)
-    hoursDiv.innerHTML = String(formatTime(hours))
-    minutesDiv.innerHTML = String(formatTime(minutes))
-    secondsDiv.innerHTML = String(formatTime(seconds))
+    daysDiv.innerHTML = days
+    hoursDiv.innerHTML = formatTime(hours)
+    minutesDiv.innerHTML = formatTime(minutes)
+    secondsDiv.innerHTML = formatTime(seconds)
 }
 
-function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
+function formatTime(time){
+    return time < 10 ? `0${time}` : time; 
 }
 
+countdown()
+
+setInterval(countdown, 1000)
--- ressources.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$servername = "localhost";
-$database = "Sapin_de_Noel";
-$username = "root";
-$password = "";
