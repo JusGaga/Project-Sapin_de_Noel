f4071fac3a40e89c016eb65b718c138094386c9a
Gaetan Jus <gaetan3107@gmail.com>
2021-12-14 16:57:37 +0100

    14/12/2021 16h57

--- JS/Compteurs.js
+++ JS/Compteurs.js
@@ -9,8 +9,6 @@ function countdown() {
   let date = new Date(date_final);
   let current_date = new Date();
 
-  // console.log("date",date,"current_date",current_date)
-
   total_seconds = (date - current_date) / 1000;
 
   const days = Math.floor(total_seconds / 3600 / 24);
@@ -19,7 +17,7 @@ function countdown() {
   const seconds = Math.floor(total_seconds) % 60;
 
   if (days < 0 && hours < 0 && minutes < 0 && seconds < 0) {
-      location.replace("../tirage.html")
+    location.replace("../tirage.html");
   } else {
     daysDiv.innerHTML = formatTime(days);
     hoursDiv.innerHTML = formatTime(hours);
